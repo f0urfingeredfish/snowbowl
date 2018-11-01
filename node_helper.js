926e56cb1699172a86867300b06ca2ee268e698c
Nick Kircos <nick@blackholedesign.com>
2018-10-31 21:30:07 -0600

    Use prettier to format, remove grunt and other eslint config

--- node_helper.js
+++ node_helper.js
@@ -8,40 +8,44 @@
 var NodeHelper = require("node_helper");
 
 module.exports = NodeHelper.create({
+  // Override socketNotificationReceived method.
 
-	// Override socketNotificationReceived method.
-
-	/* socketNotificationReceived(notification, payload)
+  /* socketNotificationReceived(notification, payload)
 	 * This method is called when a socket notification arrives.
 	 *
 	 * argument notification string - The identifier of the noitication.
 	 * argument payload mixed - The payload of the notification.
 	 */
-	socketNotificationReceived: function(notification, payload) {
-		if (notification === "snowbowl-NOTIFICATION_TEST") {
-			console.log("Working notification system. Notification:", notification, "payload: ", payload);
-			// Send notification
-			this.sendNotificationTest(this.anotherFunction()); //Is possible send objects :)
-		}
-	},
+  socketNotificationReceived: function(notification, payload) {
+    if (notification === "snowbowl-NOTIFICATION_TEST") {
+      console.log(
+        "Working notification system. Notification:",
+        notification,
+        "payload: ",
+        payload
+      );
+      // Send notification
+      this.sendNotificationTest(this.anotherFunction()); //Is possible send objects :)
+    }
+  },
 
-	// Example function send notification test
-	sendNotificationTest: function(payload) {
-		this.sendSocketNotification("snowbowl-NOTIFICATION_TEST", payload);
-	},
+  // Example function send notification test
+  sendNotificationTest: function(payload) {
+    this.sendSocketNotification("snowbowl-NOTIFICATION_TEST", payload);
+  },
 
-	// this you can create extra routes for your module
-	extraRoutes: function() {
-		var self = this;
-		this.expressApp.get("/snowbowl/extra_route", function(req, res) {
-			// call another function
-			values = self.anotherFunction();
-			res.send(values);
-		});
-	},
+  // this you can create extra routes for your module
+  extraRoutes: function() {
+    var self = this;
+    this.expressApp.get("/snowbowl/extra_route", function(req, res) {
+      // call another function
+      values = self.anotherFunction();
+      res.send(values);
+    });
+  },
 
-	// Test another function
-	anotherFunction: function() {
-		return {date: new Date()};
-	}
+  // Test another function
+  anotherFunction: function() {
+    return { date: new Date() };
+  }
 });
