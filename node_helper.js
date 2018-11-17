cebce7db0ba86e2438ed3002bcb2df343f3bc3a9
Nick Kircos <nick@blackholedesign.com>
2018-11-17 00:29:25 -0700

    Curl lost trail report

--- node_helper.js
+++ node_helper.js
@@ -6,6 +6,8 @@
  */
 
 var NodeHelper = require("node_helper");
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
 
 const getHTML = url =>
   new Promise((resolve, reject) => {
@@ -56,8 +58,11 @@ module.exports = NodeHelper.create({
         Log.error(e);
       }
       try {
-        const report = await getHTML("https://www.losttrail.com/snow-report/");
-        this.sendSocketNotification("snowbowl-GET_REPORT_LOST", report);
+        const { stdout, stderr } = await exec(
+          "curl 'https://www.losttrail.com/snow-report/' -H 'authority: www.losttrail.com' -H 'pragma: no-cache' -H 'cache-control: no-cache' -H 'upgrade-insecure-requests: 1' -H 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36' -H 'dnt: 1' -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8' -H 'referer: https://www.losttrail.com/' -H 'accept-encoding: gzip, deflate, br' -H 'accept-language: en-US,en;q=0.9' -H 'cookie: PHPSESSID=fj63rlp29ogot5tbcjsrmcev02; wcp_useroptions_id=9a66350fb22ae9459b26a24a1df4bfc15befb463d85e55.61366257; visid_incap_1728444=bHAfANHZQOeM0K8OENIuI2G071sAAAAAQUIPAAAAAABB6dYV/I+qwBHZxoBJ/hFM; incap_ses_207_1728444=E3AGL44rO1sXGDOq22nfAmO071sAAAAAZN/Z3eGilflDPIJND3hRNg==; _fbp=fb.1.1542435944295.173978093; vchideactivationmsg=1; vchideactivationmsg_vc11=5.5.5' --compressed"
+        );
+        // const report = await getHTML("https://www.losttrail.com/snow-report/");
+        this.sendSocketNotification("snowbowl-GET_REPORT_LOST", stdout);
       } catch (e) {
         Log.error(e);
       }
